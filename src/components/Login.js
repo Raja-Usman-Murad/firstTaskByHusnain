f27af321bc37cc45ed9f888701c35d5ecb21c5df
Raja Usman <usmankiyani501@gmail.com>
2022-05-25 23:10:14 +0500

    create login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,107 @@
-import React from "react";
+import React, { useState } from "react";
+import { NavLink, useNavigate } from "react-router-dom";
 
 const Login = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const history = useNavigate();
+
+    const submitData = async (e) => {
+        console.log("it is submit section");
+        e.preventDefault();
+        if (!email || !password) {
+          alert("fill all the fields");
+        } else {
+          const res = await fetch("/signin", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ email, password }),
+          });
+          const data = await res.json();
+          console.log(data);
+          if (res.status === 400 || !data) {
+            alert("invalid credentials");
+          } else {
+            alert("valid credentials");
+            history("/");
+          }
+        }
+      };
+
     return(
         <>
-        <h1 className="bg-dark">Login</h1>
+        <div className="container-fluid">
+        <div className="row">
+          <div className="col-10 mx-auto">
+            <section className="mt-5" id="text_shadow">
+              <div className="container-fluid" id="registration_main_container">
+                <div className="row">
+                  <div className="col-md-6 col-10 order-sm-1 order-md-0 d-flex flex-column align-items-center justify-content-center ">
+                    
+                    <NavLink
+                      exact
+                      className="navbar-brand text-dark"
+                      to="/signup"
+                    >
+                      Create An Account
+                    </NavLink>
+                  </div>
+                  <div className="col-md-6 col-10 order-sm-0 order-md-1">
+                    <h1 className="mt-5" id="signup-heading">
+                      Sign <span id="logohalfcolorchange">In</span>
+                    </h1>
+                    <form method="POST" className="mt-3" onSubmit={submitData}>
+                      <div className="mb-3 d-flex flex-row justify-content-center align-items-center">
+                        <div className="mr-2">
+                          <i className="zmdi zmdi-email zmdi-hc-3x mr-3"></i>
+                        </div>
+                        <input
+                          // required
+                          value={email}
+                          onChange={(e) => setEmail(e.target.value)}
+                          type="email"
+                          className="form-control"
+                          id="email"
+                          name="email"
+                          placeholder="Your Email"
+                        />
+                      </div>
+
+                      <div className="mb-3 d-flex flex-row justify-content-center align-items-center">
+                        <div className="mr-2">
+                          <i className="zmdi zmdi-lock zmdi-hc-3x mr-3"></i>
+                        </div>
+                        <input
+                          // required
+                          value={password}
+                          onChange={(e) => setPassword(e.target.value)}
+                          type="password"
+                          className="form-control"
+                          id="password"
+                          name="password"
+                          placeholder="Your Password"
+                        />
+                      </div>
+
+                      <div className="form-group">
+                        <input
+                          type="submit"
+                          name="login"
+                          value="SignIn"
+                          id="login"
+                          className="form-submit btn btn-primary p-2 btn-lg "
+                        />
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </section>
+          </div>
+        </div>
+      </div>
         </>
     )
 }
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,33 +4,33 @@ import { NavLink } from "react-router-dom";
 const Navbar = ()=> {
     return (
         <>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <NavLink class="navbar-brand" to="/">Navbar</NavLink>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <NavLink className="navbar-brand" to="/">Navbar</NavLink>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
         </button>
       
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item active ml-4">
-              <NavLink class="nav-link" to="/">Home <span class="sr-only">(current)</span></NavLink>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active ml-4">
+              <NavLink className="nav-link" to="/">Home <span className="sr-only">(current)</span></NavLink>
             </li>
-            <li class="nav-item ml-4">
-              <NavLink class="nav-link" to="/signup">Signup <span class="sr-only">(current)</span></NavLink>
+            <li className="nav-item ml-4">
+              <NavLink className="nav-link" to="/signup">Signup <span className="sr-only">(current)</span></NavLink>
             </li>
-            <li class="nav-item ml-4">
-              <NavLink class="nav-link" to="/login">Login <span class="sr-only">(current)</span></NavLink>
+            <li className="nav-item ml-4">
+              <NavLink className="nav-link" to="/login">Login <span className="sr-only">(current)</span></NavLink>
             </li>
-            <li class="nav-item ml-4">
-              <NavLink class="nav-link" to="/list">List <span class="sr-only">(current)</span></NavLink>
+            <li className="nav-item ml-4">
+              <NavLink className="nav-link" to="/list">List <span className="sr-only">(current)</span></NavLink>
             </li>
-            <li class="nav-item ml-4">
-              <NavLink class="nav-link" to="/sharelist">ShareList <span class="sr-only">(current)</span></NavLink>
+            <li className="nav-item ml-4">
+              <NavLink className="nav-link" to="/sharelist">ShareList <span className="sr-only">(current)</span></NavLink>
             </li>
           </ul>
-          <form class="form-inline my-2 my-lg-0">
-             <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          <form className="form-inline my-2 my-lg-0">
+             <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
           </form>
         </div>
       </nav>
